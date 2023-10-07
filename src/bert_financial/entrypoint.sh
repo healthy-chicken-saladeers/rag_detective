f95ec9ac2b721041aa59ace4fae67a89b3e54c43
Ian Kelk <iankelk@gmail.com>
2023-10-07 16:00:10 -0400

    Update entrypoint.sh

--- src/bert_financial/entrypoint.sh
+++ src/bert_financial/entrypoint.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 # Mount the GCS bucket
-gcsfuse ac215_scraper_bucket /app/gcsbucket
-#sudo gcsfuse -o allow_other -o uid=$(id -u appuser) -o gid=$(id -g appuser) ac215_scraper_bucket /app/gcsbucket
-
+sudo gcsfuse -o allow_other,default_permissions ac215_scraper_bucket /app/gcsbucket
 
 # Run the provided command or default to a shell
 exec "$@"
